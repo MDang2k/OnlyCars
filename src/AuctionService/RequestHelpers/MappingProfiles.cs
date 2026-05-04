5be2d7ca4b59cac0051742732e098ba9927e5181
Minh Dang Doan <s240481@th-ab.de>
2026-05-04 23:09:23 +0200

    First commit

---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,18 @@
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Item>();
+        CreateMap<CreateAuctionDto, Auction>()
+            .ForMember(d => d.Item, opt => opt.MapFrom(src => src));
+        CreateMap<UpdateAuctionDto, Auction>();
+    }
+}
